bec6c9ae77a3f32ce868975d1d2bb3b255742174
Whitney Songer <wsonger0129@gmail.com>
2025-04-12 20:35:12 -0700

    Added user input, max heart and resting rate calculations, and effort calculations

--- RoverESP/RoverESP.ino
+++ RoverESP/RoverESP.ino
@@ -1,28 +1,30 @@
 #include <WiFi.h>
 #include <esp_now.h>
-#include "BPM.h"
+#include "EFFORT.h"
 
 //Wifi Related
-bpm receivedData;
+effort receivedData;
 
 //Ports/Connections
 //Motor 1
 #define ENA 21 //Speed control pin (0, 255)
-#define IN1 20 //Backward
-#define IN2 10 //Forward
+#define IN1 20 //Forward
+#define IN2 10 //Backward
 //Motor 2
 #define ENB 3 //Speed control pin (0, 255)
-#define IN3 0 //Forward
-#define IN4 1 //Backward
+#define IN3 0 //Backward
+#define IN4 1 //Forward
 
 void onReceive(const esp_now_recv_info_t *info, const uint8_t *data, int len) {
   if (len == sizeof(receivedData)) {
     memcpy(&receivedData, data, sizeof(receivedData));
-    Serial.print("Received BPM: ");
+    Serial.print("Received Effort: ");
     Serial.println(receivedData.value);
   } else {
     Serial.println("Invalid data size.");
   }
+
+
 }
 
 void setup() {
@@ -56,11 +58,31 @@ void setup() {
   // Register receive callback with the correct function signature
   esp_now_register_recv_cb(onReceive);
 
-  analogWrite(ENA, 120);
-  analogWrite(ENB, 120);
+  // analogWrite(ENA, 120);
+  // analogWrite(ENB, 120);
 }
 
 void loop() {
-  
+  int EffortLvl = receivedData.value;
+  int speed = 0;
+
+  if (EffortLvl < 0.15) {
+    speed = 0;
+  } else if (EffortLvl >= 0.15 && EffortLvl <= 0.433) {
+    speed = 80;
+  } else if (EffortLvl > 0.433 && EffortLvl <= 0.71633) {
+    speed = 138.88;
+  } else if (EffortLvl > 0.71633) {
+    speed = 255;
+  }
+
+  analogWrite(ENA, speed);
+  analogWrite(ENB, speed);
+
+  // Drive both motors forward
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
 }
 

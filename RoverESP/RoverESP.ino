34ae05dd7d19f59dafd41da0244d889cca45a2b7
Whitney Songer <wsonger0129@gmail.com>
2025-04-12 21:30:00 -0700

    Effort speed calculation added

--- RoverESP/RoverESP.ino
+++ RoverESP/RoverESP.ino
@@ -1,9 +1,17 @@
+#include <Timer.h>
 #include <WiFi.h>
 #include <esp_now.h>
-#include "BPM.h"
+#include "EFFORT.h"
+
+
+Timer timer;
+
+// Flags
+bool timerStarted = false;
+bool stopMotors = false;
 
 //Wifi Related
-bpm receivedData;
+effort receivedData;
 
 //Ports/Connections
 //Motor 1
@@ -15,10 +23,19 @@ bpm receivedData;
 #define IN3 0 //Forward
 #define IN4 1 //Backward
 
+void stopAllMotors() {
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
 void onReceive(const esp_now_recv_info_t *info, const uint8_t *data, int len) {
   if (len == sizeof(receivedData)) {
     memcpy(&receivedData, data, sizeof(receivedData));
-    Serial.print("Received BPM: ");
+    Serial.print("Received Effort: ");
     Serial.println(receivedData.value);
   } else {
     Serial.println("Invalid data size.");
@@ -56,11 +73,44 @@ void setup() {
   // Register receive callback with the correct function signature
   esp_now_register_recv_cb(onReceive);
 
-  analogWrite(ENA, 120);
-  analogWrite(ENB, 120);
+
 }
 
 void loop() {
-  
+  if (timerStarted && timer.read() >= 30000 && !stopMotors) {
+    stopAllMotors();
+    stopMotors = true;
+    Serial.println("30 seconds elapsed. Rover stopped.");
+  }
+
+  // Skip motor control if time is up
+  if (stopMotors) return;
+
+  int EffortLvl = receivedData.value;
+  int speed = 0;
+
+  if (EffortLvl < 0.15) {
+    speed = 0;
+  } else if (EffortLvl >= 0.15 && EffortLvl <= 0.433) {
+    analogWrite(ENA, 150);
+    analogWrite(ENB, 150);
+    delay(100);
+    speed = 80;
+  } else if (EffortLvl > 0.433 && EffortLvl <= 0.71633) {
+    speed = 138.88;
+  } else if (EffortLvl > 0.71633){
+    speed = 255;
+  }
+
+  analogWrite(ENA, speed);
+  analogWrite(ENB, speed);
+
+  // Drive both motors forward
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+  delay(300);
 }
 
